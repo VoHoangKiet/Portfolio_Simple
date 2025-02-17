2b193cc075b2cc62661a0b0a23d83563e3d52177
PC <muoivo.051064@gmail.com>
2025-02-18 01:07:05 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import ImageSlider from "./components/ImageSlider";
 
+
 function App() {
   return (
-      <div className="App">
-        <div className="header">Do Minh Thinh Portfolio</div>
-        <ImageSlider />
-      </div>
+    <div className="App">
+      <div className="header">Do Minh Thinh Portfolio</div>
+      <ImageSlider />
+    </div>
   );
 }
 
